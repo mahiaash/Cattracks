02f545ff64c7a432b654795849ba9e4f035fe1a5
adammak137 <33068944+adammak137@users.noreply.github.com>
2017-11-05 11:15:31 -0500

    About us

--- app.py
+++ app.py
@@ -1,68 +1,41 @@
-from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
-from flask_cors import CORS
-import os, json, time
+from flask import Flask, render_template, request, jsonify, abort
+import os, requests, json
 # from clarifai.rest import ClarifaiApp
 # from clarifai.rest import Image as ClImage
 
 # clarifai_app = ClarifaiApp(api_key='')
 
 app = Flask(__name__)
-CORS(app)
 
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 DB = {
-    1 : {
-        'name': 'Bella',
-        'color': 'black',
-        'description': 'none',
-        'spayed_neutured': 'unknown',
-        'feral_domestic': 'unknown',
-        'points': [
-            {'image':'angry.jpg','latlng': (43.003158, -78.787532), 'timestamp': 1509768000,'desc':'angry'},
-            {'image':'derp.jpg','latlng': (43.004028, -78.785915), 'timestamp': 1509796800,'desc':'derppp'}
-        ]},
-    2 : {
-        'name': 'Sam',
-        'color': 'Silver w Black stripes',
-        'description': 'none',
-        'spayed_neutured': 'unknown',
-        'feral_domestic': 'unknown',
-        'points': [
-            {'image':'j09w6p65.jpg','latlng': (43.004278, -78.789974), 'timestamp': 1509768000,'desc':'look a my bugs'},
-            {'image':'6v1hryov.jpg','latlng': (43.006263, -78.785919), 'timestamp': 1509796800,'desc':'please don\'t feed'}
-        ]},
-      3 : {
-        'name': 'Octocat',
-        'color': 'Black',
-        'description': 'Somehow it looks like both a cat and an octopus',
-        'spayed_neutured': 'unknown',
-        'feral_domestic': 'unknown',
-        'points': [
-            {'image':'Octocat.png','latlng': (43.002700, -78.787421), 'timestamp': 1509768000,'desc':'UBHacking 2k17!'},
-           
-        ]},  
+    1 : {'name': 'Bella',},
+    2 : {'name': 'Sam'}
 }
 
+#description
+@app.route('/about')
+def about():
+    return render_template('about.html')
 #view
 @app.route('/')
-def index():
+def hello_world():
     return render_template('index.html')
 
 @app.route('/cats')
 def getCats():
     return jsonify(DB)
 
+
 #add cat
 @app.route('/cat/add', methods=['POST'])
 def addCat():
     if request.method == 'POST':
         #get cat data and insert to database
-        data = json.loads(request.data)
+        data = request.form
         new_key = max(DB.keys()) + 1
-
-        data['points'] = []
         DB[new_key] = data
         
         #return success or error message
@@ -78,30 +51,24 @@ def getCatInfo(cat_id):
     else:
         return abort(404)
 
-#add picture or location
-@app.route('/cat/<int:cat_id>/add', methods=['POST'])
-def addPoint(cat_id):
-    if cat_id in DB and request.method == 'POST':
-        #save image
-        file = request.files['image']
-        filename = file.filename
-        if filename:
-            f = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(f)
-        else:
-            filename = ''
 
-        timestamp = time.time()
-        data = request.form
-        
-        DB[cat_id]['points'].append({'image': filename,
-                                    'latlng': (float(data['latitude']), float(data['longitude'])), 
-                                    'timestamp': timestamp,
-                                    'desc': data['description']})
-        # return jsonify(DB[cat_id])
-        return redirect(url_for('index'))
-    else:
-        return abort(404)
+
+#upload image example from https://stackoverflow.com/questions/28982974/flask-restful-upload-image
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    
+    # add your custom code to check that the uploaded file is a valid image and not a malicious file (out-of-scope for this post)
+
+    file.save(f)
+
+    #check, if there is a cat in the photo
+    # model = app.models.get('general-v1.3')
+    # image = ClImage(file_obj=open('/home/user/image.jpeg', 'rb'))
+    # model.predict([image])
+
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
